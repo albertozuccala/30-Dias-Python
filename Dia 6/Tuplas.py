2d7600d5b577cf20e36f8592e09284910a504c7a
albertozuccala <albertozuccala@gmail.com>
2023-09-29 17:07:37 -0300

    29/09

---
+++ Dia 6/Tuplas.py
@@ -0,0 +1,9 @@
+######## Tuplas #######
+# Una tupla es una colección de diferentes tipos de datos ordenados e inmutables (inmutables). 
+# Las tuplas se escriben con corchetes, (). Una vez que se crea una tupla, no podemos cambiar sus valores. 
+# No podemos usar métodos de agregar, insertar, eliminar en una tupla porque no es modificable (mutable). 
+# A diferencia de list, tuple tiene pocos métodos. Métodos relacionados con las tuplas:
+
+empty_tuple = ()
+# o usando el constructor de tuplas
+empty_tuple = tuple()
