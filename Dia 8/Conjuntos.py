53c002dafbc3807ab5a32c74cc80138d03ad3cdc
albertozuccala <albertozuccala@gmail.com>
2023-10-03 17:01:19 -0300

    03-10

--- Dia 8/Conjuntos.py
+++ Dia 8/Conjuntos.py
@@ -31,3 +31,35 @@ for beatle in beatles:
 
 #Ordenando un conjunto, usamos la funcion sorted() porque sort() NO FUNCIONA
 print(sorted(beatles)) #Obtenemos una Lista con los elementos ordenados ya que los conjuntos no mantienen un orden
+
+#Teoría de conjuntos
+A= {1,2}
+B={2,3}
+
+#Intersección
+print(A.intersection(B))
+#Unión
+print(A.union(B))
+#Diferencia
+print(A.difference(B))
+#Diferencia simétrica
+print(A.symmetric_difference(B))
+#Inclusión
+A = {2, 4, 6, 8, 10}
+B = {4, 6, 8}
+print(B < A) #subconjunto
+print(B<=A)
+print(A> B)  #superconjunto
+print(A>=B)
+
+#Conjuntos por comprensión
+#aquellos números enteros múltiplos de 3 en el rango (0, 20)
+numeros = {numero for numero in range(0,20) if numero % 3 == 0}
+print(numeros)
+
+#Conjuntos inmutables
+#Python ofrece la posibilidad de crear conjuntos inmutables haciendo uso de la función frozenset()
+marcas = [1, 3, 2, 3, 1, 4, 2, 4, 5, 2, 5, 5, 3, 1, 4]
+marcas = frozenset(marcas)
+print(marcas)
+#marcas.add(50)  NO es posible modificar un conjunto frozenset()
